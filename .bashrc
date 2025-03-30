a21acc6360144d2ea5ef0ff94b226e05580757ce
angelluvslots <vvoid127@gmail.com>
2025-03-29 21:51:06 -0700

    a lot

--- .bashrc
+++ .bashrc
@@ -1,1 +1,2 @@
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias rebuild='sudo nixos-rebuild switch --flake $HOME/nixos#angel'
--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,17 +1,28 @@
 {
-  description = "Nixos config flake";
+  description = "angel's nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # spicetify-nix = {
+    #   url = "github:Gerg-L/spicetify-nix";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./configuration.nix
-      ];
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.angel = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+        ];
+      };
     };
-  };
 }
