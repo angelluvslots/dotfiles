5518b46ea74d978ff086da9df2118cec08cce1a8
angelluvslots <vvoid127@gmail.com>
2025-03-29 21:53:08 -0700

    add new nix files

---
+++ nixos/display.nix
@@ -0,0 +1,42 @@
+{ config, ... }:
+
+{
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable wayland with gdm
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  # services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = { layout = "us"; variant = ""; };
+
+  # Enable hyprland
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  hardware = {
+    graphics.enable = true;
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = false;
+      powerManagement.finegrained = false;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
+      prime = {
+        offload.enable = false;
+        sync.enable = true;
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+  };
+}
---
+++ nixos/packages.nix
@@ -0,0 +1,56 @@
+{ inputs, pkgs, ... }:
+
+let
+  # spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
+  # spicetify = inputs.spicetify-nix.lib.mkSpicetify pkgs {
+    # enable = true;
+    # theme = spicePkgs.themes.Gruvbox;
+  # };
+in
+{
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    ghostty
+    obsidian
+    discord
+    spotify
+    # spicetify
+    inputs.zen-browser.packages."${system}".default
+
+    # Cli
+    neovim
+    git
+    github-cli
+    neofetch
+    lshw
+
+    # Language
+    gleam
+    rustup
+    lua
+    luajit
+    gcc
+    python3
+
+    # Hyprland
+    kitty
+    waybar
+    rofi-wayland
+
+    # Gnome
+    # gtk4
+    # gnomeExtensions.forge
+    # gnomeExtensions.open-bar
+  ];
+
+  # OLD WAY
+  # fonts.packages = with pkgs; [
+  #   (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  #   jetbrains-mono
+  # ];
+  fonts.packages = with pkgs; [
+    pkgs.nerd-fonts.jetbrains-mono
+  ];
+}
